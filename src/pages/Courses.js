1d60cdf62041b3091775da58d99a96dfe978792e
Phudit-Sigmaboi <phudit7008za@gmail.com>
2024-12-27 11:45:42 +0700

    be sigma
    so sigma

---
+++ src/pages/Courses.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function Courses() {
+  return (
+    <section id="courses" style={styles.section}>
+      <h2>Courses</h2>
+      <p>ด้านล่างนี้คือเอกสารหลักสูตร:</p>
+      <iframe
+        src="public/31901v5.pdf"
+        title="PDF Viewer"
+        style={styles.iframe}
+      />
+    </section>
+  );
+}
+
+const styles = {
+  section: {
+    padding: "2em",
+    textAlign: "center",
+  },
+  iframe: {
+    width: "100%",
+    height: "600px",
+    border: "1px solid #ccc",
+  },
+};
+
+export default Courses;
